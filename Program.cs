40778c9dbe3fcd6192eea9aa12f25e63591e124c
Jonathan Bodrero <jbodrero@hotmail.com>
2025-07-03 13:00:02 -0600

    Intro and map

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//  Jonathan Bodrero
+//  Jul 3, 2025
+//  Lab 8:  Maze 1
+
+Console.Clear();
+Console.WriteLine("Welcome to our aMAZEing game.");
+Console.WriteLine("Use the arrow keys to move your character from top left to the * symbol.\n");
+
+string[] mapRows = File.ReadAllLines("map.txt");
+foreach (string row in mapRows)
+{
+    Console.WriteLine(row);
+}

b7cd5b3bf5991c02843dcae92393e0546cc58365
Jonathan Bodrero <jbodrero@hotmail.com>
2025-07-03 13:48:50 -0600

    Read key from user.

--- Program.cs
+++ Program.cs
@@ -4,10 +4,40 @@
 
 Console.Clear();
 Console.WriteLine("Welcome to our aMAZEing game.");
-Console.WriteLine("Use the arrow keys to move your character from top left to the * symbol.\n");
+Console.WriteLine("Use the arrow keys to move your character from top left to the * symbol.\n To exit, press Enter key.\n");
+Console.WriteLine("Press any key when ready.");
+Console.ReadKey(true);
+Console.Clear();
+
+int locFromLeft = 0;
+int locFromTop = 0;
+ConsoleKeyInfo input;
+char mazeChar = ' ';
+
 
 string[] mapRows = File.ReadAllLines("map.txt");
+
 foreach (string row in mapRows)
 {
     Console.WriteLine(row);
 }
+//  Start character at top left
+
+Console.SetCursorPosition(0, 0);
+Console.Write("@");
+ConsoleKeyInfo keyIn;
+do
+{
+    keyIn = Console.ReadKey(true);
+  
+    if (keyIn.Key == ConsoleKey.Escape)
+    {
+        Console.WriteLine("Thanks for playing.  Goodbye.");
+        break;
+                
+    }
+    
+    
+
+}
+while (keyIn.Key != ConsoleKey.Escape);
--- obj/Debug/net9.0/lab-7-maze-JBodrero-1.AssemblyInfo.cs
+++ obj/Debug/net9.0/lab-7-maze-JBodrero-1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-7-maze-JBodrero-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4a0a22de9c0ebd71cbd03eab5a81ad5d988bb4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40778c9dbe3fcd6192eea9aa12f25e63591e124c")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-7-maze-JBodrero-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-7-maze-JBodrero-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
